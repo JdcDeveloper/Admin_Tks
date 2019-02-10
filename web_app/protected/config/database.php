90a31b52b62452f8a62908d3ed7a3ed96575638a
jonathan <jonathancastro2018@yahoo.com>
2019-02-10 00:09:51 -0400

    borrando carpetas y archivos innecesarios

--- web_app/protected/config/database.php
+++ web_app/protected/config/database.php
@@ -4,11 +4,11 @@
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+
+	'connectionString' => 'pgsql:host=localhost;port=5432;dbname=Admin_Tks',	
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
 	'charset' => 'utf8',
-	*/
+	
 );
