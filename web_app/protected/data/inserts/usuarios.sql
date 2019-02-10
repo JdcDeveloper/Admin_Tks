af5f7d5b2a2b737d80c4a6f1b508244b911ae000
jonathan <jonathancastro2018@yahoo.com>
2019-02-10 19:14:24 -0400

    agregando base de datos y algunos inserts

---
+++ web_app/protected/data/inserts/usuarios.sql
@@ -0,0 +1,30 @@
+INSERT INTO jonathan.usuarios
+(nombre,email,password,estado,municipio,parroquia,role,fecha)
+VALUES ('jonathan','admin@hotmail.com',md5('123'),'distrito federal','libertador','san jose','admin', '2016-06-22 19:10:25-07');
+
+
+INSERT INTO jonathan.usuarios
+(nombre,email,password,estado,municipio,parroquia,role,fecha)
+VALUES ('maria','maria@hotmail.com',md5('123'),'distrito federal','libertador','quinta crespo','user', '2017-06-22 19:10:25-07');
+
+
+INSERT INTO jonathan.usuarios
+(nombre,email,password,estado,municipio,parroquia,role,fecha)
+VALUES ('jose','jose@hotmail.com',md5('123'),'distrito federal','libertador','candelaria','user', '2018-06-22 19:10:25-07');
+
+
+INSERT INTO jonathan.usuarios
+(nombre,email,password,estado,municipio,parroquia,role,fecha)
+VALUES ('cristobal','cristobal@hotmail.com',md5('123'),'distrito federal','libertador','san juan','user', '2017-06-22 19:10:25-07');
+
+INSERT INTO jonathan.usuarios
+(nombre,email,password,estado,municipio,parroquia,role,fecha)
+VALUES ('pacheco','pacheco@hotmail.com',md5('123'),'distrito federal','libertador','san martin','user', '2017-06-22 19:10:25-07');
+
+INSERT INTO jonathan.usuarios
+(nombre,email,password,estado,municipio,parroquia,role,fecha)
+VALUES ('gonzalo','gonzalo@hotmail.com',md5('123'),'distrito federal','libertador','altagracia','user', '2017-06-22 19:10:25-07');
+
+INSERT INTO jonathan.usuarios
+(nombre,email,password,estado,municipio,parroquia,role,fecha)
+VALUES ('cristina','cristina@hotmail.com',md5('123'),'distrito federal','libertador','23 de enero','user', '2017-06-22 19:10:25-07');
